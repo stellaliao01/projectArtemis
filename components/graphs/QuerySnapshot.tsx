c133b050cfa7c4069111da60832ff0efbd35be59
Taylor Morgan <Taylormorgan882@gmail.com>
2020-11-25 13:59:50 -0800

    QuerySnap Component updated

--- components/graphs/QuerySnapshot.tsx
+++ components/graphs/QuerySnapshot.tsx
@@ -1,5 +1,4 @@
 import React from 'https://esm.sh/react';
-import { Bar } from 'https://cdn.skypack.dev/react-chartjs-2';
 import '../../style/graphs.css';
 
 interface Props {
@@ -9,48 +8,33 @@ interface Props {
 const QuerySnapshot = (props: Props) => {
 	const { queryData } = props;
 
-	// const data = {
-	// 	labels: queryData.map((obj, index) => {
-	// 		const key = `Query ${index}`;
-	// 		return key;
-	// 	}),
-	// 	datasets: [
-	// 		{
-	// 			label: 'Query Speed',
-	// 			backgroundColor: 'rgba(255,99,132,0.2)',
-	// 			borderColor: 'rgba(255,99,132,1)',
-	// 			borderWidth: 1,
-	// 			hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-	// 			hoverBorderColor: 'rgba(255,99,132,1)',
-	// 			data: queryData.map((obj: any) => {
-	// 				const value = obj.latency;
-	// 				return value;
-	// 			})
-	// 		}
-	// 	]
-	// };
+	 return (
+		<div className="query-speed-container">
+  <table>
+   <thead>
+     <tr>
+       <th>API NAME</th>
+       <th>LATENCY</th>
+       <th>DATASIZE</th>
+       <th>REQUESTED FIELDS</th>
+       <th>SUCCESSFUL QUERY</th>
 
-	// const options = {
-	// 	responsive: true,
-	// 	maintainAspectRatio: false,
-	// 	title: {
-	// 		display: true,
-	// 		text: 'Latency'
-	// 	},
-	// 	onClick: function(e, item) {
-	// 		console.log(item);
-	// 	}
-	// };
-
-	// const legend = {
-	// 	display: false
-	// };
-
-	// return (
-	// 	<div className="query-speed-container">
-	// 		<Bar data={data} options={options} legend={legend} aria-label="display-graph-query-speeds" role="img" />
-	// 	</div>
-	// );
+     </tr>
+   </thead>
+  <tbody>
+    <tr>
+      <td>{queryData[2].api}</td>
+      <td>{queryData[2].latency}</td>
+      <td>{queryData[2].dataSize}</td>
+      <td>{queryData[2].requestedFields[2]}</td>
+      <td>true</td>
+    </tr>
+  </tbody>
+   </table>
+	 	</div>
+	);
 };
 
+
 export default QuerySnapshot;
+
