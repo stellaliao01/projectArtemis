15b1517085f71a636e8c09efb023a3e9c1deac0d
Taylor Morgan <Taylormorgan882@gmail.com>
2020-12-03 18:44:49 -0800

    table graph updated

--- components/analyticsComponents/graphs/QuerySnapshot.tsx
+++ components/analyticsComponents/graphs/QuerySnapshot.tsx
@@ -1,41 +1,39 @@
 import React from 'https://esm.sh/react';
 import '../../../style/graphs.css';
 
+
 interface Props {
 	queryData: [];
 }
 
-const QuerySnapshot = (props: Props) => {
+ const QuerySnapshot = (props: Props) => {
 	const { queryData } = props;
 
-	 return (
-		<div className="graph">
-      {/* hello */}
-  <table>
-   <thead>
+ 	 return (
+	<div className="graph">
+    <table className="table table-striped">
+  <thead>
+      <tr style={{width: '50%'}}>
+       <th style={{width: '50%'}}>API NAME</th>
+        <th style={{width: '50%'}}>LATENCY</th>
+        <th style={{width: '50%'}}>DATASIZE</th>
+       <th style={{width: '50%'}}>REQUESTED FIELDS</th>
+        <th style={{width: '50%', color: 'red'}}>SUCCESSFUL QUERY</th>
+      </tr>
+    </thead>
+   <tbody>
      <tr>
-       <th>API NAME</th>
-       <th>LATENCY</th>
-       <th>DATASIZE</th>
-       <th>REQUESTED FIELDS</th>
-       <th>SUCCESSFUL QUERY</th>
-
-     </tr>
-   </thead>
-  <tbody>
-    <tr>
-      {/* <td>{queryData[2].api}</td>
-      <td>{queryData[2].latency}</td>
-      <td>{queryData[2].dataSize}</td>
-      <td>{queryData[2].requestedFields[2]}</td>
-      <td>true</td> */}
-    </tr>
-  </tbody>
+     <td>hi</td>
+     <td>im</td>
+     <td>here</td>
+    <td>foo</td>
+  </tr>
+   </tbody>
    </table>
-	 	</div>
+</div>
 	);
-};
-
+ };
+//  <td>{queryData[2].latency}</td>
 //make table from scratch to be cssed up
 //make query data iterable and reusable
 
